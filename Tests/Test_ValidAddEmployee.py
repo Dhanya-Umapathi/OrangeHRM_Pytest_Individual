6482531c20f631aa76c8bcd52638ba3be25dd929
= <=>
2024-07-16 13:57:53 +0530

    commit on main

---
+++ Tests/Test_ValidAddEmployee.py
@@ -0,0 +1,27 @@
+import pytest,time
+from Pages import PIMPage
+from Utility import Excel_Reader,console_logger
+from Tests import Test_ValidLogin
+
+@pytest.mark.usefixtures("setup_and_teardown")
+@pytest.mark.parametrize("fname,mname,lname,emp_id", Excel_Reader.get_data("E:\gitpytest_clone\Pytest_OrangeHRM\ExcelFiles\\Add_Employee.xlsx", "AddEmployee"))
+
+
+class TestValidAddEmployee:
+    log = console_logger.get_logger()
+    @pytest.mark.smoke
+    def test_validAddEmp(self, fname, mname, lname,emp_id):
+        Test_ValidLogin.TestLogin.test_valid_login(self)
+        self.log.info("Login is successful")
+        pim = PIMPage.PIM_Page(self.driver)
+        pim.PIM_button()
+        self.log.info("PIM button is clicked")
+        pim.add_employee_button()
+        self.log.info("Add Employee button is clicked")
+        pim.add_employee(fname, mname, lname,emp_id)
+        self.log.info("First_name,Middle_name,Last_name are sent to add value")
+        pim.save_button()
+        self.log.info("Save button is clicked")
+        pim.success_message()
+        self.log.info("Successfull added")
+        
---
+++ Tests/Test_searchreport.py
@@ -0,0 +1,26 @@
+import pytest
+from Pages import PIMPage
+from Utility import Excel_Reader,console_logger
+from Tests import Test_ValidLogin
+
+@pytest.mark.usefixtures("setup_and_teardown")
+
+class TestReport:
+    log = console_logger.get_logger()
+    @pytest.mark.smoke
+    @pytest.mark.parametrize("report_name", Excel_Reader.get_data("E:\gitpytest_clone\Pytest_OrangeHRM\ExcelFiles\Report.xlsx", "Report"))
+    def test_searchreport(self, report_name):
+        self.driver.implicitly_wait(10)
+        Test_ValidLogin.TestLogin.test_valid_login(self)
+        self.log.info("Login is successfull")
+        pim = PIMPage.PIM_Page(self.driver)
+        pim.PIM_button()
+        self.log.info("PIM button is clicked")
+        pim.report_button()
+        self.log.info("report button is clicked")
+        pim.report_search(report_name)
+        self.log.info("report needed to searched is given")
+        pim.search()
+        self.log.info("Report is searched")
+        pim.valid_assert()
+        self.log.info("Report is fetched successfully")
